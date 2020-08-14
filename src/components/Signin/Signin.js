a4f9e185f696e86e9461bd2ec9fd2beb107f82bf
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-08-15 05:27:06 +0530

    commiting changes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignin = () => {
-    fetch('http://localhost:3000/signin',{
+    fetch('https://mighty-earth-04737.herokuapp.com/signin',{
       method:'post',
       headers: { 'Content-Type' : 'application/json'},
       body: JSON.stringify({
